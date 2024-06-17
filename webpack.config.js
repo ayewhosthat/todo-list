96a43d46c3fb98dbb4aae10d4a382a10143bab26
ayewhosthat <alan.notiverson@gmail.com>
2024-06-16 21:53:48 -0400

    Modify webpack config to include htmlplugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: 
-    [
-    new HtmlWebpackPlugin({
-      title: 'Todo List',
-      template: './src/index.html',
-    }),
-    ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  plugins: 
+    [new HtmlWebpackPlugin({
+      title: 'Todo List',
+      template: './src/index.html',
+    }),
+    ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
