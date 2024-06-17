9ec39e6c6364b768ce04ea42187634bf8244cd99
ayewhosthat <alan.notiverson@gmail.com>
2024-06-17 19:20:56 -0400

    Show project name modal upon clicking 'new project'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+// function to clear the page
+const main = document.querySelector('.main');
+function clearPage() {
+    main.innerHTML = '';
+}
+
+// add event listener to 'new project' button
+const newProject = document.querySelector('.add-project');
+const projectModal = document.querySelector('.project-modal');
+newProject.addEventListener('click', () => {
+    // show modal
+    projectModal.showModal();
+});
